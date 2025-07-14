3813150ac90cd3e35333674f2b304ad063edc7df
anup4747 <anupdtarwade@gmail.com>
2025-07-14 22:01:04 +0530

    added vite react

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+
+import './App.css'
+
+function App() {
+
+  return (
+   <section className='h-full w-full flex justify-center items-center'>
+      <h1 className='text-white underline'>WAR OF THRONE</h1>
+   </section>
+  )
+}
+
+export default App
